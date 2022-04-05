7c49b78e54538f6e6c528279d762c1f74e65c0f0
Said <SSAID@jaguarlandrover.com>
2022-04-05 14:22:26 +0100

    unittest
    test models

--- store/tests/test_models.py
+++ store/tests/test_models.py
@@ -7,7 +7,7 @@ from ..models import *
 class TestCategoryModel(TestCase):
 
     def setUp(self):
-        self.data1 = Category.objects.create(name='django',slug='django')
+        self.data1 = Category.objects.create(name='django',slug='django',id=5)
         
 
     def test_category_model_entry(self):
@@ -24,3 +24,19 @@ class TestCategoryModel(TestCase):
         
         self.assertEqual(str(self.data1),'django')
 
+
+class TestProductModel(TestCase):
+    def setUp(self):
+        self.data1 = Category.objects.create(name='django',slug='django',id=5)
+        self.user = User.objects.create(first_name='hassan')
+        self.data2 = Product.objects.create(title='GCSE',quantity=50,price=44,created_by=self.user,category_id=5)
+
+    
+    def test_product_model_entry(self):
+        
+        """
+        Test Product model data insertion/types/field attributes
+         """
+        data = self.data2
+        self.assertTrue(isinstance(data,Product))
+        self.assertEqual(str(data),'GCSb')
