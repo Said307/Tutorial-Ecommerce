10e23118978c80cb77a63551fe43d022a8af9099
sadiq said <sadiq-said@hotmail.com>
2022-04-16 13:08:41 +0100

    basket

--- basket/views.py
+++ basket/views.py
@@ -14,7 +14,8 @@ def basket_add(request):
     basket = Basket(request)
     if request.POST.get("action") == "basket_key":
         product_id = int(request.POST.get("productid"))
+        product_qty = int(request.POST.get("productqty"))
         product = get_object_or_404(Product, id=product_id)
-        basket.add(product=product)
+        basket.add(product=product, qty=product_qty)
 
-    return JsonResponse({"test": "testdata"})
+    return JsonResponse({"qty": product_qty})
