b5f6c8a356c4b9e74f6ce03280e0a6802b25e09d
sadiq said <sadiq-said@hotmail.com>
2022-04-19 11:45:18 +0100

    session

--- basket/basket.py
+++ basket/basket.py
@@ -1,3 +1,8 @@
+from decimal import Decimal
+
+from store.models import Product
+
+
 class Basket:
     """A basket class providing default behaviours which
     can be inherited or overriden as necessary"""
@@ -20,3 +25,23 @@ class Basket:
             }
 
         self.session.modified = True
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty of items
+        """
+        return sum(item["qty"] for item in self.basket.values())
+
+    def __iter__(self):
+        """iterate over the session data of
+        basket items and fetch the from the database"""
+        product_ids = self.basket.keys()
+        products = Product.active_products.filter(id__in=product_ids)
+        basket = self.basket.copy()
+        for product in products:
+            basket[str(product.id)]["product"] = product
+
+        for item in basket.values():
+            item["price"] = Decimal(item["price"])
+            item["total_price"] = item["price"] * item["qty"]
+            yield
--- basket/views.py
+++ basket/views.py
@@ -5,7 +5,7 @@ from .basket import Basket
 
 
 def basket_summary(request):
-
+    basket = Basket(request)
     context = {}
     return render(request, "store/basket/summary.html", context)
 
@@ -17,5 +17,30 @@ def basket_add(request):
         product_qty = int(request.POST.get("productqty"))
         product = get_object_or_404(Product, id=product_id)
         basket.add(product=product, qty=product_qty)
+        basket_qty = basket.__len__()
+    return JsonResponse({"qty": basket_qty})
 
-    return JsonResponse({"qty": product_qty})
+
+def basket_delete(request):
+    basket = Basket(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        basket.delete(product=product_id)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({"qty": basketqty, "subtotal": baskettotal})
+        return response
+
+
+def basket_update(request):
+    basket = Basket(request)
+    if request.POST.get("action") == "post":
+        product_id = int(request.POST.get("productid"))
+        product_qty = int(request.POST.get("productqty"))
+        basket.update(product=product_id, qty=product_qty)
+
+        basketqty = basket.__len__()
+        baskettotal = basket.get_total_price()
+        response = JsonResponse({"qty": basketqty, "subtotal": baskettotal})
+        return
