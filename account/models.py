1a002b84b29476c4d47609573745317626c8026f
Said <sadiq-said@hotmail.com>
2022-04-25 12:50:43 +0100

    user model added

---
+++ account/models.py
@@ -0,0 +1,41 @@
+
+from django.db import models
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
+                                    PermissionsMixin)
+
+from django_countries.fields import CountryField
+ 
+from django.contrib.auth.models import Model
+
+class UserBase(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(_('email address'), unique=True)
+    user_name = models.CharField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=150, blank=True)
+    about = models.TextField(_(
+        'about'), max_length=500, blank=True)
+    # Delivery details
+    country = CountryField()
+    phone_number = models.CharField(max_length=15, blank=True)
+    postcode = models.CharField(max_length=12, blank=True)
+    address_line_1 = models.CharField(max_length=150, blank=True)
+    address_line_2 = models.CharField(max_length=150, blank=True)
+    town_city = models.CharField(max_length=150, blank=True)
+    # User Status
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = CustomAccountManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['user_name']
+
+    class Meta:
+        verbose_name = "Accounts"
+        verbose_name_plural = "Accounts"
+
+
+    def __str__(self):
+        return self.user_name
