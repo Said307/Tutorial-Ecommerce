2a5feb977038737ff238dc4f51e80fba475f85c5
Said <sadiq-said@hotmail.com>
2022-04-27 14:13:50 +0100

    new database

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import UserBase
+
+
+@admin.register(UserBase)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["pk","first_name","email"]
+     
